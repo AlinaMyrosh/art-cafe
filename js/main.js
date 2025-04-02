0b4dc87200824505078368caa3052d068201caca
AlinaMyrosh <154742742+AlinaMyrosh@users.noreply.github.com>
2025-04-02 20:45:10 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-// document.querySelectorAll(".pagination button").forEach((button) => {
-//   button.addEventListener("click", function () {
-//     document
-//       .querySelectorAll(".image-gallery img")
-//       .forEach((img) => img.classList.remove("active"));
-//     const targetImage = document.getElementById(this.dataset.target);
-//     targetImage.classList.add("active");
-//   });
-// });
+const buttons = document.querySelector(".btn-container");
+const allEvents = document.querySelector(".all-events");
+const heroTitle = document.querySelector(".hero-title");
+
+buttons.addEventListener("click", onClick);
+
+function checkVisibility() {
+  const rect = heroTitle.getBoundingClientRect();
+  if (rect.top < window.innerHeight && rect.bottom > 0) {
+    heroTitle.classList.add("visible");
+  }
+}
+
+window.addEventListener("scroll", checkVisibility);
+window.addEventListener("load", checkVisibility);
+
+function onClick(event) {
+  const currentEvent = event.target.dataset.target;
+  allEvents.querySelectorAll(".event").forEach((div) => {
+    div.classList.add("visually-hidden");
+  });
+
+  buttons.querySelectorAll(".events-btn").forEach((item) => {
+    item.classList.replace("is-active", "not-active");
+  });
+
+  console.log(buttons.querySelectorAll(".event"));
+
+  allEvents
+    .querySelector(`.${currentEvent}`)
+    .classList.remove("visually-hidden");
+
+  event.target.classList.replace("not-active", "is-active");
+  console.log(event.target.classList);
+}
